f82c3207569d6dcc085df058bdd4a5785054ee87
Arun Vijo Tharakan <108383137+arunvijo04@users.noreply.github.com>
2024-06-01 17:23:23 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    new Typed('#animated-text', {
-        strings: ['Web Developer', 'Tech Enthusiast', 'Lifelong Learner'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        loop: true
-    });
-});
